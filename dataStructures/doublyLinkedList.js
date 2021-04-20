127b79efdaae46636c8e8c1c3702b3d583725758
tedo74 <teddy2@abv.bg>
2021-04-20 20:20:32 +0300

    doubly linked list push method

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -12,4 +12,34 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    // if list is empty
+    const newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
 }
+
+const list = new DoublyLinkedList();
+console.log(list.push(1));
+console.log(list.push(2));
+console.log(list.push(3));
+console.log('************************');
+list.traverse();
