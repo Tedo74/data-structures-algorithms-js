8db59baca98a78e78493e6169d2d852a7ce84b75
tedo74 <teddy2@abv.bg>
2021-04-22 11:40:13 +0300

    doubly linked list pop method

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -28,6 +28,25 @@ class DoublyLinkedList {
     return this;
   }
 
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let itemToReturn = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prevNode = this.tail.prev;
+      prevNode.next = null;
+      this.tail = prevNode;
+    }
+    this.length--;
+    return itemToReturn;
+  }
+
   traverse() {
     let current = this.head;
     while (current) {
@@ -38,8 +57,17 @@ class DoublyLinkedList {
 }
 
 const list = new DoublyLinkedList();
-console.log(list.push(1));
-console.log(list.push(2));
-console.log(list.push(3));
+// console.log(list.push(1));
+// console.log(list.push(2));
+// console.log(list.push(3));
+list.push(1);
+list.push(2);
+list.push(3);
+list.traverse();
+console.log('************************');
+console.log(list.pop());
+console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.pop());
 console.log('************************');
 list.traverse();
