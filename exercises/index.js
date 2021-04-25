7ea4e3f41183af7d3b95da5444cc9076cd6af66a
tedo74 <teddy2@abv.bg>
2021-04-25 12:52:43 +0300

    exercises included and string reverse

---
+++ exercises/index.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    // solution 1
+    // let returnStr=""
+    // for (let i=str.length-1; i >= 0; i--){
+    //     returnStr += str[i];
+    // }
+    // return returnStr;
+
+    // solution 2
+    return [...str].reverse().join('');
+}
+console.log(reverse("abc   *"));
